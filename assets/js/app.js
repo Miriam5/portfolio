0bc17567f0e666703bc8c2fd3b9db17222395fc5
miriam <miriam5000@gmail.com>
2017-08-09 10:38:59 -0400

    major revise, with flexbox working

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	anim();
+	anim2();
+});
+
+var anim = function(){//MiriamP
+	//$(".move").hide().show('slide', {direction: 'left'}, "slow")};
+	$('.fade').hide(0).delay(300).fadeIn(2000)};
+
+var anim2 = function(){//footer text
+	$('.blue').hide(0).delay(500).fadeIn(3000)};
+
+//how might I use js to create the six boxes? Look at notes from last sat night, modularity. 
+//teaching -- hover, Wissahickon Charter School, Teacher Workshops at Springside CH
